858ca5b5fc384450b895edd631c1e66340270844
Swus1978 <98677463+Swus1978@users.noreply.github.com>
2024-01-14 13:10:54 -0500

    adding banner and pictures to the restaurant with PHP coding

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!doctype html>
+i<!doctype html>
 <html lang="en">
     <head>
         <title>Title</title>
@@ -16,18 +16,16 @@
             integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
             crossorigin="anonymous"
         />
-
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     </head>
-
     <body>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
-        <a class="navbar-brand" href="#">Restaurante La Sombra</a>
+        <a class="navbar-brand" href="#"><i class="fas fa-utensils"></i> Restaurante La Sombra</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
     aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
         </button>
-
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                     <li class="nav-item">
@@ -48,30 +46,75 @@
             </ul>
         </div>
         </div>
-
         </nav>
+        <section id="banner" class="container-fluid p-0">
+            <div class="banner-img" style="position:relative; background:url('images/pngtree-summer-atmosphere-restaurant-supermarket-vegetable-psd-layered-promotion-background-image_159939.jpg') center/cover no-repeat; height:400px;">
+                <div class="banner-text" style="position:absolute; top:50%; left:50%; transform: translate(-50%, -50%); text-align:center; color: #fff;">
+                    <h1>La Sombra</h1>
+                    <p>Restaurante del mejor sabor casero</p>
+                    <a href="#menu" class="btn btn-primary">Ver Menu</a>
+                </div>
+            </div>
+        </section>
+        <section id="id" class="container mt-4 text-center">
+        <div class="jumbotron bg-dark text-white">
+            <br/>
+                <h2>¡Bienvenid@s al Restaurant La Sombra!</h2>
+                <p>Descubre una experiencia culinaria unica</p>
+            <br/>
+        </div>
+</section>
+<section id="chefs" class="container mt-4 text-center">
+    <h2>Nuestros Chefs</h2>
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card">
+                <img src="images/Chef-in-uniform-posing-for-a-photo-in-a-kitchen-1400.jpg" class="card-img-top" alt="Chef 1" />
+                <div class="card-body">
+                    <h5 class="card-title">Chef 1</h5>
+                    <p class="card-text">El chef Juan Carlos</p>
+                    <div class="social-icons mt-3">
+                        <a href="#" class="text-dark mt-2"><i class="fab fa-facebook"></i></a>
+                        <a href="#" class="text-dark mt-2"><i class="fab fa-instagram"></i></a>
+                        <a href="#" class="text-dark mt-2"><i class="fab fa-linkedin"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <img src="images/colaboradores/gringachef.jpg" class="card-img-top" alt="Chef 2" />
+                <div class="card-body">
+                    <h5 class="card-title">Chef 2</h5>
+                    <p class="card-text">La chef Amy</p>
+                    <div class="social-icons mt-3">
+                        <a href="#" class="text-dark mt-2"><i class="fab fa-facebook"></i></a>
+                        <a href="#" class="text-dark mt-2"><i class="fab fa-instagram"></i></a>
+                        <a href="#" class="text-dark mt-2"><i class="fab fa-linkedin"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <img src="images/colaboradores/mujerblackchef.jpeg" class="card-img-top" alt="Chef 3" />
+                <div class="card-body">
+                    <h5 class="card-title">Chef 3</h5>
+                    <p class="card-text">La chef Maria</p>
+                    <div class="social-icons mt-3">
+                        <a href="#" class="text-dark mt-2"><i class="fab fa-facebook"></i></a>
+                        <a href="#" class="text-dark mt-2"><i class="fab fa-instagram"></i></a>
+                        <a href="#" class="text-dark mt-2"><i class="fab fa-linkedin"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-        <header>
-            <!-- place navbar here -->
-        </header>
-        <main>
-
-            Parte Central
-
-
-        </main>
-
-
-
-
-
-        <footer class="bg-dark text-light text-center">
-            <p>&copy; 2024 Restaurante La Sombra, todos los derechos reservados.</p>
-        </footer>
-
-
-
-
+    </div>
+</section>
+    <footer class="bg-dark text-light text-center">
+        <p>&copy; 2024 Restaurante La Sombra, todos los derechos reservados.</p>
+    </footer>
         <!-- Bootstrap JavaScript Libraries -->
         <script
             src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
