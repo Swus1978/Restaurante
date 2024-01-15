b4aded5c22d9dc177c2bac44650d6e7de8b9a11d
Swus1978 <98677463+Swus1978@users.noreply.github.com>
2024-01-14 19:38:41 -0500

    updated more files

--- admin/index.php
+++ admin/index.php
@@ -1,1 +1,25 @@
-<php  include("section/header.php");?>
+<?php include("plates/header.php"); ?>
+<div class="row align-items-md-stretch">
+    <div class="col-md-6">
+        <div
+            class="h-100 p-5 text-white  border rounded-3"
+        >
+            <h2>Change the background</h2>
+            <p>
+                Swap the background-color utility and add a `.text-*` color
+                utility to mix up the jumbotron look. Then, mix and match with
+                additional component themes and more.
+            </p>
+            <button
+                class="btn btn-outline-primary"
+                type="button"
+            >
+                Example button
+            </button>
+        </div>
+    </div>
+</div>
+
+
+<?php include("plates/footer.php"); ?>
+
